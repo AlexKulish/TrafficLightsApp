5513fe6f48f069d063a043e9fc50a704784f93a5
Alexey Kulish <alexkulish1993@gmail.com>
2021-12-08 20:23:14 +0300

    Setup logic for switching lights

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    var counterOfClick = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        startButton.setTitle("START", for: .normal)
+        startButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func pressedButton() {
+        startButton.setTitle("NEXT", for: .normal)
+        counterOfClick += 1
+        
+        redLightView.layer.cornerRadius = redLightView.layer.bounds.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.layer.bounds.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.layer.bounds.width / 2
+        
+        if counterOfClick % 3 == 1 {
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        } else if counterOfClick % 3 == 2 {
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+        } else if counterOfClick % 3 == 0 {
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+        }
+    }
 }
 
